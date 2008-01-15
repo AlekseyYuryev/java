00019c1ef49d7c0d0576aba8531367eb110b31e2
Seva Safris <seva@safris.org>
2008-01-15 23:35:19 +0000

    Adding tests for the sample examples.

---
+++ xml/sample/binding/src/test/java/org/safris/xml/toolkit/sample/binding/EnumsTest.java
@@ -0,0 +1,24 @@
+package org.safris.xml.toolkit.sample.binding;
+
+import org.safris.xml.toolkit.sample.binding.AbstractTest;
+import org.safris.xml.toolkit.sample.binding.AnyTest;
+import org.safris.xml.toolkit.sample.binding.any.AnyTrash;
+import org.safris.xml.toolkit.sample.binding.enums.EnumsColorAttr;
+import org.safris.xml.toolkit.sample.binding.enums.EnumsColoredFruitBasket;
+import org.safris.xml.toolkit.sample.binding.simple.ISimpleFruitType;
+import org.safris.xml.toolkit.sample.binding.simple.SimpleFruit;
+import org.safris.xml.toolkit.sample.binding.simple.SimpleFruitBasket;
+import org.safris.xml.toolkit.sample.binding.xsitype.ITypeFleshyFruitType;
+import org.safris.xml.toolkit.sample.binding.xsitype.ITypeIndehiscentDryFruitType;
+
+public class EnumsTest extends AbstractTest
+{
+	public static void main(String[] args)
+	{
+		new EnumsTest().testExample();
+	}
+
+	public void testExample()
+	{
+	}
+}
---
+++ xml/sample/binding/src/test/java/org/safris/xml/toolkit/sample/binding/XsiTypeTest.java
@@ -0,0 +1,24 @@
+package org.safris.xml.toolkit.sample.binding;
+
+import org.safris.xml.toolkit.sample.binding.AbstractTest;
+import org.safris.xml.toolkit.sample.binding.AnyTest;
+import org.safris.xml.toolkit.sample.binding.any.AnyTrash;
+import org.safris.xml.toolkit.sample.binding.enums.EnumsColorAttr;
+import org.safris.xml.toolkit.sample.binding.enums.EnumsColoredFruitBasket;
+import org.safris.xml.toolkit.sample.binding.simple.ISimpleFruitType;
+import org.safris.xml.toolkit.sample.binding.simple.SimpleFruit;
+import org.safris.xml.toolkit.sample.binding.simple.SimpleFruitBasket;
+import org.safris.xml.toolkit.sample.binding.xsitype.ITypeFleshyFruitType;
+import org.safris.xml.toolkit.sample.binding.xsitype.ITypeIndehiscentDryFruitType;
+
+public class XsiTypeTest extends AbstractTest
+{
+	public static void main(String[] args)
+	{
+		new XsiTypeTest().testExample();
+	}
+
+	public void testExample()
+	{
+	}
+}
