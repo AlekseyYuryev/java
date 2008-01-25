ea7d473c96b897db3a853e2657e6356244f8321e
Seva Safris <seva@safris.org>
2008-01-25 20:46:27 +0000

    Closed #6 with an implementation of ConsoleFormatter that doesn't use the GetPropertyAction class.

--- commons/util/src/main/java/org/safris/commons/util/logging/ConsoleFormatter.java
+++ commons/util/src/main/java/org/safris/commons/util/logging/ConsoleFormatter.java
@@ -1,18 +1,15 @@
 package org.safris.commons.util.logging;
 
-import java.security.AccessController;
-import java.security.PrivilegedAction;
 import java.text.MessageFormat;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
-import sun.security.action.GetPropertyAction;
 
 public class ConsoleFormatter extends Formatter
 {
 	// Line separator string.  This is the value of the line.separator
 	// property at the moment that the SimpleFormatter was created.
-	private final static GetPropertyAction lineSeparator = AccessController.<GetPropertyAction>doPrivileged(new GetPrivilegedPropertyAction(new GetPropertyAction("line.separator")));
-	private final static String format = "{0,date} {0,time}";
+	private static final String format = "{0,date} {0,time}";
+	private static final String newline = System.getProperty("line.separator");
 	private final MessageFormat formatter;
 
 	public ConsoleFormatter()
@@ -27,29 +24,14 @@ public class ConsoleFormatter extends Formatter
 	 */
 	public String format(LogRecord record)
 	{
-		synchronized(lineSeparator)
+		synchronized(formatter)
 		{
 			final StringBuffer buffer = new StringBuffer();
 			final String message = formatMessage(record);
 //			buffer.append("[").append(record.getLevel().getLocalizedName()).append("] ");
 			buffer.append(message);
-			buffer.append(lineSeparator.run());
+			buffer.append(newline);
 			return buffer.toString();
-		}
-	}
-
-	private static final class GetPrivilegedPropertyAction implements PrivilegedAction<GetPropertyAction>
-	{
-		private final GetPropertyAction getPropertyAction;
-
-		public GetPrivilegedPropertyAction(GetPropertyAction getPropertyAction)
-		{
-			this.getPropertyAction = getPropertyAction;
-		}
-
-		public GetPropertyAction run()
-		{
-			return getPropertyAction;
 		}
 	}
 }
