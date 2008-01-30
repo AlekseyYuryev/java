936d677186cff5c1abf00ad13ba53496e33777ad
Seva Safris <seva@safris.org>
2008-01-30 01:29:11 +0000

    This addresses #15: these 2 files were omitted in the last commit.

---
+++ xml/generator/lexer/src/main/java/org/safris/xml/generator/lexer/phase/composite/SchemaComposite.java
@@ -0,0 +1,7 @@
+package org.safris.xml.generator.lexer.phase.composite;
+
+import org.safris.xml.generator.module.phase.ElementModule;
+
+public interface SchemaComposite extends ElementModule
+{
+}
