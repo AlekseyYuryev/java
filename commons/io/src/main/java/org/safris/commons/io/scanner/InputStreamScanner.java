330b0f103efef9d9b81161475bef4e92f8e461d5
Seva Safris <seva@safris.org>
2008-02-08 00:27:00 +0000

    InputStreamScanner is now a Thread. This closes #37.

--- commons/io/src/main/java/org/safris/commons/io/scanner/InputStreamScanner.java
+++ commons/io/src/main/java/org/safris/commons/io/scanner/InputStreamScanner.java
@@ -3,88 +3,72 @@ package org.safris.commons.io.scanner;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-import org.safris.commons.io.scanner.ScannerHandler;
 import org.safris.commons.util.HashTree;
 
-public class InputStreamScanner
+public class InputStreamScanner extends Thread
 {
 	private final InputStream in;
+	private final HashTree<ScannerHandler> tree;
 	private List<HashTree.Node<ScannerHandler>> currentNodes;
 
-	public InputStreamScanner(InputStream in)
+	public InputStreamScanner(InputStream in, HashTree<ScannerHandler> tree)
 	{
 		this.in = in;
+		this.tree = tree;
+		currentNodes = tree != null ? tree.getChildren() : null;
 	}
 
-	public void setScript(HashTree<ScannerHandler> tree)
+	private void doMatch(String line, List<HashTree.Node<ScannerHandler>> nodes) throws IOException
 	{
-		currentNodes = tree.getChildren();
-	}
-
-	public void scrape()
-	{
-		new ScrapingThread(in).start();
-	}
-
-	private class ScrapingThread extends Thread
-	{
-		private final InputStream in;
-
-		public ScrapingThread(InputStream in)
+		for(HashTree.Node<ScannerHandler> node : nodes)
 		{
-			this.in = in;
-		}
-
-		private void doMatch(String line, List<HashTree.Node<ScannerHandler>> nodes) throws IOException
-		{
-			for(HashTree.Node<ScannerHandler> node : nodes)
+			if(node.getValue() != null)
 			{
-				if(node.getValue() != null)
+				if(line.matches(node.getValue().getMatch()))
 				{
-					if(line.matches(node.getValue().getMatch()))
-					{
-						node.getValue().match(line);
-						if(node.hasChildren())
-							currentNodes = node.getChildren();
-					}
+					node.getValue().match(line);
+					if(node.hasChildren())
+						currentNodes = node.getChildren();
 				}
+			}
+			else
+			{
+				for(HashTree.Node<ScannerHandler> child : node.getChildren())
+					doMatch(line, child.getChildren());
+			}
+		}
+	}
+
+	public void run()
+	{
+		String line = "";
+		try
+		{
+			char ch = 0;
+			while((ch = (char)in.read()) != -1)
+			{
+				if(ch == '\n')
+					line = "";
 				else
-				{
-					for(HashTree.Node<ScannerHandler> child : node.getChildren())
-						doMatch(line, child.getChildren());
-				}
+					line += ch;
+
+				if(currentNodes == null)
+					continue;
+
+				doMatch(line, currentNodes);
 			}
 		}
-
-		public void run()
+		catch(Exception e)
 		{
-			String line = "";
-			try
-			{
-				char ch = 0;
-				while((ch = (char)in.read()) != -1)
-				{
-					if(ch == '\n')
-						line = "";
-					else
-						line += ch;
+			// FIXME: Have to NOT System.exit() here.
+			if("Pipe broken".equals(e.getMessage()))
+				System.exit(0);
 
-					if(currentNodes == null)
-						continue;
-
-					doMatch(line, currentNodes);
-				}
-			}
-			catch(Exception e)
-			{
-				if("Pipe broken".equals(e.getMessage()))
-				{
-					//					System.err.println(e.getMessage());
-					System.exit(0);
-				}
-
-				throw new RuntimeException(e);
-			}
+			throw new RuntimeException(e);
+		}
+		finally
+		{
+			notifyAll();
 		}
 	}
 }
