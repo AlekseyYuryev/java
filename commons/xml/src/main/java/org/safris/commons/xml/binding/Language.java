c4d5b27d9158fb8b49881fb3374d6d2801689f2a
Seva Safris <seva@safris.org>
2008-09-15 23:41:02 +0000

    Implemented the Language binding, including LanguageTest, closing #136

--- commons/xml/src/main/java/org/safris/commons/xml/binding/Language.java
+++ commons/xml/src/main/java/org/safris/commons/xml/binding/Language.java
@@ -15,40 +15,112 @@
 
 package org.safris.commons.xml.binding;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
+/**
+ * http://www.w3.org/TR/xmlschema11-2/#language
+ */
 public class Language implements CharSequence
 {
 	public static Language parseLanguage(String string)
 	{
-		return instances.get(string);
+		if(string == null)
+			throw new NullPointerException("string == null");
+
+		string = string.trim();
+		if(string.length() < LANGUAGE_FRAG_MIN_LENGTH)
+			throw new IllegalArgumentException(string);
+
+		final StringTokenizer tokenizer = new StringTokenizer(string, "-");
+		final Collection<String> languages = new ArrayList<String>();
+		while(tokenizer.hasMoreTokens())
+			languages.add(tokenizer.nextToken());
+
+		return new Language(languages);
 	}
 
-	private static final Map<String,Language> instances = new HashMap<String,Language>();
-	private final String value;
+	private static final Pattern firstPattern = Pattern.compile("[a-zA-Z]{1,8}");
+	private static final Pattern otherPattern = Pattern.compile("[a-zA-Z0-9]{1,8}");
+	private static final int LANGUAGE_FRAG_MIN_LENGTH = 1;
+	private final String[] language;
+	private String encoded = null;
 
-	public Language(String value)
+	public Language(String ... language)
 	{
-		this.value = value;
-		instances.put(value, this);
+		if(language == null)
+			throw new NullPointerException("language == null");
+
+		if(language.length == 0)
+			throw new IllegalArgumentException("language.length == 0");
+
+		if(!firstPattern.matcher(language[0]).matches())
+			throw new IllegalArgumentException(language[0]);
+
+		for(int i = 1; i < language.length; i++)
+			if(!otherPattern.matcher(language[i]).matches())
+				throw new IllegalArgumentException(language[i]);
+
+		this.language = language;
+	}
+
+	public Language(Collection<String> language)
+	{
+		this(language != null ? language.toArray(new String[language.size()]) : null);
+	}
+
+	public String[] getLanguage()
+	{
+		return language;
 	}
 
 	public int length()
 	{
-		// TODO: Implement this method
-		return 0;
+		return toString().length();
 	}
 
 	public char charAt(int index)
 	{
-		// TODO: Implement this method
-		return 0;
+		return toString().charAt(index);
 	}
 
 	public CharSequence subSequence(int start, int end)
 	{
-		// TODO: Implement this method
-		return null;
+		return toString().subSequence(start, end);
+	}
+
+	public boolean equals(Object obj)
+	{
+		if(obj == this)
+			return true;
+
+		if(!(obj instanceof Year))
+			return false;
+
+		final Language that = (Language)obj;
+		return this.language != null ? Arrays.equals(this.language, that.language) : that.language == null;
+	}
+
+	public int hashCode()
+	{
+		return language != null ? Arrays.hashCode(language) : -1;
+	}
+
+	public String toString()
+	{
+		if(encoded != null)
+			return encoded;
+
+		if(language == null || language.length == 0)
+			return encoded = "";
+
+		final StringBuffer buffer = new StringBuffer();
+		for(String string : language)
+			buffer.append("-").append(string);
+
+		return encoded = buffer.substring(1);
 	}
 }
---
+++ commons/xml/src/test/java/org/safris/commons/xml/binding/LanguageTest.java
@@ -0,0 +1,187 @@
+package org.safris.commons.xml.binding;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LanguageTest
+{
+	public static void main(String[] args)
+	{
+		new LanguageTest().testLanguage();
+	}
+
+	@Test
+	public void testLanguage()
+	{
+		try
+		{
+			Language.parseLanguage(null);
+			fail("Expected a NullPointerException");
+		}
+		catch(NullPointerException e)
+		{
+		}
+
+		try
+		{
+			Language.parseLanguage("");
+			fail("Expected a IllegalArgumentException");
+		}
+		catch(IllegalArgumentException e)
+		{
+		}
+
+		try
+		{
+			Language.parseLanguage("11");
+			fail("Expected a IllegalArgumentException");
+		}
+		catch(IllegalArgumentException e)
+		{
+		}
+
+		try
+		{
+			Language.parseLanguage("superlong");
+			fail("Expected a IllegalArgumentException");
+		}
+		catch(IllegalArgumentException e)
+		{
+		}
+
+		try
+		{
+			Language.parseLanguage("witha1");
+			fail("Expected a IllegalArgumentException");
+		}
+		catch(IllegalArgumentException e)
+		{
+		}
+
+		try
+		{
+			Language.parseLanguage("witha-another#");
+			fail("Expected a IllegalArgumentException");
+		}
+		catch(IllegalArgumentException e)
+		{
+		}
+
+		try
+		{
+			Language.parseLanguage("witha-and-another#");
+			fail("Expected a IllegalArgumentException");
+		}
+		catch(IllegalArgumentException e)
+		{
+		}
+
+		try
+		{
+			Language.parseLanguage("721-fhajdo-f1h");
+			fail("Expected a IllegalArgumentException");
+		}
+		catch(IllegalArgumentException e)
+		{
+		}
+
+		final String[] languages = new String[]
+		{
+			"art-lojban",
+			"az-Arab",
+			"az-Cyrl",
+			"az-Latn",
+			"be-Latn",
+			"bs-Cyrl",
+			"bs-Latn",
+			"cel-gaulish",
+			"de-1901",
+			"de-1996",
+			"de-AT-1901",
+			"de-AT-1996",
+			"de-CH-1901",
+			"de-CH-1996",
+			"de-DE-1901",
+			"de-DE-1996",
+			"en-boont",
+			"en-GB-oed",
+			"en-scouse",
+			"es-419",
+			"i-ami",
+			"i-bnn",
+			"i-default",
+			"i-enochian",
+			"i-hak",
+			"i-klingon",
+			"i-lux",
+			"i-mingo",
+			"i-navajo",
+			"i-pwn",
+			"i-tao",
+			"i-tay",
+			"i-tsu",
+			"iu-Cans",
+			"iu-Latn",
+			"mn-Cyrl",
+			"mn-Mong",
+			"no-bok",
+			"no-nyn",
+			"sgn-BE-fr",
+			"sgn-BE-nl",
+			"sgn-BR",
+			"sgn-CH-de",
+			"sgn-CO",
+			"sgn-DE",
+			"sgn-DK",
+			"sgn-ES",
+			"sgn-FR",
+			"sgn-GB",
+			"sgn-GR",
+			"sgn-IE",
+			"sgn-IT",
+			"sgn-JP",
+			"sgn-MX",
+			"sgn-NL",
+			"sgn-NO",
+			"sgn-PT",
+			"sgn-SE",
+			"sgn-US",
+			"sgn-ZA",
+			"sl-rozaj",
+			"sr-Cyrl",
+			"sr-Latn",
+			"tg-Arab",
+			"tg-Cyrl",
+			"uz-Cyrl",
+			"uz-Latn",
+			"yi-latn",
+			"zh-cmn",
+			"zh-cmn-Hans",
+			"zh-cmn-Hant",
+			"zh-gan",
+			"zh-guoyu",
+			"zh-hakka",
+			"zh-Hans",
+			"zh-Hans-CN",
+			"zh-Hans-HK",
+			"zh-Hans-MO",
+			"zh-Hans-SG",
+			"zh-Hans-TW",
+			"zh-Hant",
+			"zh-Hant-CN",
+			"zh-Hant-HK",
+			"zh-Hant-MO",
+			"zh-Hant-SG",
+			"zh-Hant-TW",
+			"zh-min",
+			"zh-min-nan",
+			"zh-wuu",
+			"zh-xiang",
+			"zh-yue"
+		};
+
+		for(String language : languages)
+			assertEquals(language, Language.parseLanguage(language).toString());
+	}
+}
