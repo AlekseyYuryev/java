0f4a7bc5b5da181dbe69f3d54022a1cb5a467d68
Seva Safris <seva@safris.org>
2012-02-15 03:24:42 +0000

    Added another layer of indirection for JPA Entity inheritance so that we work around JPA bugs related to selecting from tables of supertype classes only. re #188.

--- xml/test/binding/src/test/java/org/safris/xml/toolkit/test/binding/AbstractTest.java
+++ xml/test/binding/src/test/java/org/safris/xml/toolkit/test/binding/AbstractTest.java
@@ -1,10 +1,10 @@
 /*  Copyright Safris Software 2008
- *  
+ *
  *  This code is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -27,8 +27,7 @@ import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 
 public abstract class AbstractTest {
-  static
-  {
+  static {
     final Validator validator = new BindingValidator();
     Validator.setSystemValidator(validator);
   }
