458f918d173f450a178a85209c1142c6ff3b19b7
Seva Safris <seva@safris.org>
2016-02-10 13:33:15 -0800

    Addition of "jetty" module, and refactor of "logging." Version increase on many modules.

---
+++ commons/logging/src/main/java/org/safris/commons/logging/Logging.java
@@ -0,0 +1,21 @@
+package org.safris.commons.logging;
+
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Logging {
+  public static void setLevel(final Level globalLevel, final List<$lg_setting> settings) {
+    final Logger rootLogger = Logger.getLogger("");
+    rootLogger.setLevel(globalLevel);
+    for (final Handler handler : rootLogger.getHandlers())
+      handler.setLevel(globalLevel);
+
+    for (final $lg_setting setting : settings)
+      Logger.getLogger(setting._name$().text()).setLevel(Level.parse(setting._level$().text()));
+  }
+
+  private Logging() {
+  }
+}
