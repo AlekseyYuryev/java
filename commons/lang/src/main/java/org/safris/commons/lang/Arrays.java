9c148f878f08fbce056e88b857b8167be7c11cc8
Seva Safris <seva@safris.org>
2016-06-15 01:29:35 +0700

    Added an Arrays.toString(array, delimiter) utility method.

--- commons/lang/src/main/java/org/safris/commons/lang/Arrays.java
+++ commons/lang/src/main/java/org/safris/commons/lang/Arrays.java
@@ -31,6 +31,26 @@ public final class Arrays {
     return array;
   }
 
+  public static String toString(final Object[] array, String delimiter) {
+    if (array == null)
+      return null;
+
+    if (delimiter == null)
+      delimiter = "";
+
+    if (array.length == 0)
+      return "";
+
+    if (array.length == 1)
+      return String.valueOf(array[0]);
+
+    final StringBuilder buffer = new StringBuilder(String.valueOf(array[0]));
+    for (int i = 1; i < array.length; i++)
+      buffer.append(delimiter).append(String.valueOf(array[i]));
+
+    return buffer.toString();
+  }
+
   // FIXME: This has bugs!
   @SafeVarargs
   public static <T>T[] addAll(final T[] ... array) {
