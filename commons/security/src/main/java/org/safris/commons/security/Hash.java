af9404fa445a112e2e19e92725e3845a758bf17f
Seva Safris <seva@safris.org>
2016-12-23 20:49:06 -0800

    Make MessageDigest ThreadLocal.

--- commons/security/src/main/java/org/safris/commons/security/Hash.java
+++ commons/security/src/main/java/org/safris/commons/security/Hash.java
@@ -1,15 +1,15 @@
 /* Copyright (c) 2009 Seva Safris
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * You should have received a copy of The MIT License (MIT) along with this
  * program. If not, see <http://opensource.org/licenses/MIT/>.
  */
@@ -19,23 +19,33 @@ package org.safris.commons.security;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import org.safris.commons.util.Hexadecimal;
+public enum Hash {
+  MD2("MD2"),
+  MD5("MD5"),
+  SHA1("SHA-1"),
+  SHA224("SHA-224"),
+  SHA256("SHA-256"),
+  SHA384("SHA-384"),
+  SHA512("SHA-512");
 
-public final class SHA1 {
-  private static MessageDigest messageDigest = null;
+  private ThreadLocal<MessageDigest> messageDigest = null;
 
-  static {
-    try {
-      messageDigest = MessageDigest.getInstance("SHA");
-    }
-    catch (final NoSuchAlgorithmException e) {
-      throw new ExceptionInInitializerError(e);
-    }
+  Hash(final String algorithm) {
+    messageDigest = new ThreadLocal<MessageDigest>() {
+      @Override
+      protected MessageDigest initialValue() {
+        try {
+          return MessageDigest.getInstance(algorithm);
+        }
+        catch (final NoSuchAlgorithmException e) {
+          throw new ExceptionInInitializerError(e);
+        }
+      }
+    };
   }
 
-  public static Hexadecimal encode(final String string) {
-    messageDigest.update(string.getBytes());
-    final byte[] digest = messageDigest.digest();
-    return new Hexadecimal(digest);
+  public byte[] encode(final String string) {
+    messageDigest.get().update(string.getBytes());
+    return messageDigest.get().digest();
   }
 }
